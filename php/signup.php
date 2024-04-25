2783cb366a519316eec42db56f97c468f6384047
Aaronverdoold <146937567+Aaronverdoold@users.noreply.github.com>
2024-04-25 12:47:43 +0200

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -6,4 +6,8 @@ $username = $_POST['gebruikersnaam'];
 $email = $_POST['email'];
 $password = $_POST['wachtwoord'];
 insertData($username, $email, $password);
+
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+insertData($username, $email, $hashedPassword)
 ?>
